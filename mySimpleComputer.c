a67adae0a5d9ccdaefa4a26ab8c4258a92ceb1cc
Valuablegrbg <kanoplanov@mail.ru>
2022-02-04 05:05:13 +0700

    add some functions

--- mySimpleComputer.c
+++ mySimpleComputer.c
@@ -18,12 +18,28 @@ int sc_memoryInit ()
 int sc_memorySet (int address, int value)
 {
 	if (address < 0 || address >= 100){
+		flags[OUT_OF_MEMORY_BORDERS] = 1;
 		return 1;
 	}
 	memory[address] = value;
 }
-int sc_memoryGet (int address, int * value);
-int sc_memorySave (char * filename);
+
+int sc_memoryGet (int address, int * value)
+{
+	if (address < 0 || address >= 100){
+		flags[OUT_OF_MEMORY_BORDERS] = 1;
+		return 1;
+	}
+	*value = memory[address];
+}
+
+int sc_memorySave (char * filename)
+{
+	FILE *output = NULL;
+	output = fopen(filename, "wb");
+	fwrite(memory, sizeof(int), 100, output);
+	fclose(output)
+}
 int sc_memoryLoad (char * filename);
 int sc_regInit ();
 int sc_regSet (int reg, int value);
