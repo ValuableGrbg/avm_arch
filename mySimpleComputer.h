09229514b857895c63879895aaa8e1e609175f16
Valuablegrbg <kanoplanov@mail.ru>
2022-02-04 04:56:54 +0700

    Add register flags

--- mySimpleComputer.h
+++ mySimpleComputer.h
@@ -1,6 +1,13 @@
 #ifndef HEADER
 #define HEADER
 
+#define ZERO_DIVISION = 1
+#define OVERFLOW = 2
+#define OUT_OF_MEMORY_BORDERS = 3
+#define IGNORE_TICKS = 4
+#define WRONG_COMMAND = 5
+
+
 int sc_memoryInit ();
 int sc_memorySet (int address, int value);
 int sc_memoryGet (int address, int * value);
